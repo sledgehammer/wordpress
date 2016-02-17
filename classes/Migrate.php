246cd9f71454b9fb1726e088a87c5cf48dfbaba3
Bob Fanger <bfanger@gmail.com>
2016-02-17 12:04:28 +0100

    Improved ExportPost

---
+++ classes/Migrate.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Sledgehammer\Wordpress;
+
+use Sledgehammer\Object;
+
+class Migrate extends Object {
+
+    /**
+     * Create or fetch existing taxonomy.
+     * (also increases count)
+     *
+     * @param string $type  'category', 'post_tag'
+     * @param string $title  The term
+     * @param string [$slug]  The slug of the term
+     * @return \Generated\TermTaxonomy
+     */
+    function importTaxonomy($type, $title, $slug = null) {
+        $slug = $slug ?: sanitize_title($title);
+        $repo = \Sledgehammer\getRepository();
+        $term = $repo->oneTerm(['slug' => $slug], true);
+        if ($term) {
+            foreach ($term->taxonomy as $taxonomy) {
+                if ($taxonomy->taxonomy === $type) {
+                    $taxonomy->count++;
+                    return $taxonomy;
+                }
+            }
+            // the given slug belongs to another taxonomy type.
+            return static::importTaxonomy($type, $title, $slug . '-' . $type);
+        }
+        $taxonomy = $repo->createTermTaxonomy([
+            'taxonomy' => $type,
+            'count' => '1'
+        ]);
+        $term = $repo->createTerm([
+            'name' => $title,
+            'slug' => $slug,
+            'taxonomy' => [$taxonomy]
+        ]);
+        $repo->saveTerm($term);
+        return $taxonomy;
+    }
+
+    function importCategory($title, $slug = null) {
+        return static::importTaxonomy('category', $title, $slug);
+    }
+
+    function importTag($title, $slug = null) {
+        return static::importTaxonomy('post_tag', $title, $slug);
+    }
+}
--- utils/classes/ExportPost.php
+++ utils/classes/ExportPost.php
@@ -4,7 +4,6 @@ namespace Sledgehammer\Wordpress;
 
 use Sledgehammer\Dump;
 use Sledgehammer\Form;
-use Sledgehammer\Html;
 use Sledgehammer\Input;
 use Sledgehammer\Util;
 
@@ -44,26 +43,39 @@ class ExportPost extends Util
         if ($values) {
             $id = $values['id'] === 'custom' ? $values['custom'] : $values['id'];
             $post = $repo->getPost($id);
-            $meta = $post->getMeta();
+            $defaults = $repo->createPost();
             $php = "\n\n\$post = \$repo->createPost([\n";
             foreach (get_object_vars($post) as $property => $value) {
-                if (in_array($property, ['author', 'id', 'meta','taxonomies'])) {
+                if (in_array($property, ['author', 'id', 'meta','taxonomies', 'guid', 'parent_id'])) {
                     continue;
+                }
+                if ($defaults->$property === $value) {
+                    continue; // skip default values
                 }
                 $php .= "    '".$property."' => ".var_export($value, true).",\n";
             }
             $php .= "]);\n";
             $php .= "\$post->author = \$repo->oneUser(['login' => ".var_export($post->author->login, true)."]);\n";
+            if ($post->parent_id !== '0') {
+                $parent = $repo->getPost($post->parent_id);
+                $php .= "\$post->parent_id = \$repo->onePost(['type' => ".var_export($parent->type, true).", 'slug' => ".var_export($parent->slug, true)."])->id;\n";
+            }
             $php .= "\$post->setMeta(".var_export($post->getMeta(), true).");\n";
             foreach ($post->taxonomies as $taxonomy) {
                 if (count($taxonomy->term->getMeta()) !== 0 || $taxonomy->parent_id !== '0' || $taxonomy->description !== '' ||  $taxonomy->term->group !== '0' || $taxonomy->order !== '0') {
                     throw new \Exception('@todo Implement taxonomy feature');
                 }
-                $php .= "\$post->taxonomies[] = \$importer->importTaxonomy(".var_export($taxonomy->taxonomy, true).", ".var_export($taxonomy->term->name, true).", ".var_export($taxonomy->term->slug, true).");\n";
+                $php .= "\$post->taxonomies[] = Migrate::importTaxonomy(".var_export($taxonomy->taxonomy, true).", ".var_export($taxonomy->term->name, true).", ".var_export($taxonomy->term->slug, true).");\n";
             }
-            $php .= "\$repo->savePost(\$post);\n";
-            if ($post->type !== 'attachment') {
-                $php .= "\$post->guid = str_replace(".$post->id.", \$post->id, \$post->guid);\n";
+            if ($post->type == 'attachment') {
+                $php .= "\$post->guid = ".var_export($post-guid, true).";\n";
+                $php .= "\$repo->savePost(\$post);\n";
+            } else {
+                $guid = var_export($post->guid, true);
+                $guid = str_replace("'".WP_HOME, 'WP_HOME.\'', $guid);
+                $guid = str_replace("=".$post->id."'", "='.\$post->id", $guid);
+                $php .= "\$repo->savePost(\$post);\n";
+                $php .= "\$post->guid = ".$guid.";\n";
                 $php .= "\$repo->savePost(\$post, ['ignore_relations' => true]);\n";
             }
             $php .= "\n\n";
